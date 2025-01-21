f4deae30bf6dfc3b262cc2a58e46137257a00f4d
Yoonkyung Park <ygpark@nextree.io>
2025-01-21 16:21:58 +0900

    vue 3 기본

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted, watch } from "vue";
+import ChildComp from "./components/ChildComp.vue";
 
 let count = ref(0); // 상태변수 초기값(state)
 let title = "Hello Vue 3";
+let color = ref("green");
+
+onMounted(() => {
+  console.log("mounted");
+});
+
+const increaseCount = () => {
+  count.value++; // 상태변수 변경할때 .value 사용, Vue 2의 this.count++
+};
+
+watch(color, () => {});
 </script>
 
 <template>
-  <div>
+  <div class="wrap">
     <h1>{{ title }}</h1>
-    <button>Count++</button>
+    <button @click="increaseCount">Count++</button>
+    <p>{{ count }}</p>
+    <ChildComp :color="color" bgColor="gold" />
+    <button @click="color = 'red'">change color</button>
   </div>
 </template>
 
---
+++ src/components/ChildComp.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>Child Component</h2>
+    <p :style="{ color: color, backgroundColor: bgColor }">{{ color }}</p>
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  color: String,
+  bgColor: String,
+});
+// defineProps: 부모 컴포넌트로부터 전달받은 props를 사용할 때 사용
+</script>
+<style></style>
