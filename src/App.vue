3db8be60714c67fc90af3a0e8922b6cd30150329
Yoonkyung Park <ygpark@nextree.io>
2025-01-21 15:05:00 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from "vue";
+
+let count = ref(0); // 상태변수 초기값(state)
+let title = "Hello Vue 3";
+</script>
+
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <button>Count++</button>
+  </div>
+</template>
+
+<style scoped></style>
